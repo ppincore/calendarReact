8e0d3aed47ccb3a5a861583a98315aa351a7b4fc
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-05-18 00:04:08 +0300

    chore: ts to tsx

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "react";
+import Login from "../pages/LoginPage/Login";
+import Event from "../pages/EventPage/Event";
+import { NotFound } from "../pages/notFount/notFound";
+export interface IRoute {
+  path: string;
+  component: ReactNode;
+}
+
+export enum RouteNames {
+  LOGIN = "/login",
+  EVENT = "/",
+  NOT_FOUND = "*",
+}
+
+export const publicRoutes: IRoute[] = [
+  { path: RouteNames.LOGIN, component: <Login /> },
+  { path: RouteNames.NOT_FOUND, component: <NotFound /> },
+];
+export const privateRoutes: IRoute[] = [{ path: RouteNames.EVENT, component: <Event /> }];
