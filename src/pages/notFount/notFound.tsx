358898301e7add0122c637b1759380151f20b863
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-05-18 00:03:50 +0300

    feat: add "go back" button

--- src/pages/notFount/notFound.tsx
+++ src/pages/notFount/notFound.tsx
@@ -1,3 +1,12 @@
 import { type FC } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const NotFound: FC = () => <h3>Страница не найдена. Ошибка 404.</h3>;
+export const NotFound: FC = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <h3>Страница не найдена. Ошибка 404.</h3>
+      <button onClick={() => navigate(-1)}>Назад</button>
+    </>
+  );
+};
