00310a102c77901b00f688ee72ffe420e9eb2983
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-05-18 00:02:55 +0300

    fix: fix navigate in protected route

--- src/components/AppRouter/ProtectedRoute.tsx
+++ src/components/AppRouter/ProtectedRoute.tsx
@@ -1,23 +1,19 @@
-import { type ReactElement } from "react";
+import { type ReactNode } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
 interface IProtectedRouteProps {
-  children: ReactElement;
-  unAuth?: boolean;
+  children: ReactNode;
 }
 
 const ProtectedRoute = (props: IProtectedRouteProps) => {
-  const { children, unAuth } = props;
+  const { children } = props;
   const location = useLocation();
-  const isAuth = false;
+  const isAuth = false; // Здесь должна быть ваша логика проверки аутентификации
 
-  if (unAuth) {
+  if (!isAuth) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
-  console.log(!isAuth && !unAuth)
-  if (!isAuth && !unAuth) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
+
   return children;
 };
 
