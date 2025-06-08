98e18b160efdaf6072ed76873e6888d92d56ff42
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-06-08 23:10:54 +0300

    feat: add login for to login page

--- src/pages/LoginPage/Login.tsx
+++ src/pages/LoginPage/Login.tsx
@@ -1,7 +1,15 @@
-import React, { type FC } from "react";
+import { Layout, Row } from "antd";
+import  { type FC } from "react";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 const Login: FC = () => {
-  return <div>Login</div>;
+  return (
+    <Layout>
+      <Row justify="center" align="middle" className="h100">
+        <LoginForm/>
+      </Row>
+    </Layout>
+  );
 };
 
 export default Login;
