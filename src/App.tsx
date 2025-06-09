ca4371b3868d190a1712cbd912ea4650e39efdab
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-06-09 11:51:51 +0300

    feat: add auto login after start App

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,21 @@ import "./App.css";
 import AppRouter from "./components/AppRouter/AppRouter";
 import { Layout } from "antd"; // Правильный импорт Layout
 import NavBar from "./components/NavBar/NavBar";
-
+import { useEffect } from "react";
+import { useDispatch } from "./store/store";
+import { fetchUser } from "./slices/sliceStorage/userSlice";
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (localStorage.getItem("userName") && localStorage.getItem("auth")) {
+      dispatch(
+        fetchUser({
+          username: localStorage.getItem("userName")!,
+          password: localStorage.getItem("password")!,
+        })
+      );
+    }
+  }, []);
   return (
     <Layout>
       <NavBar />
