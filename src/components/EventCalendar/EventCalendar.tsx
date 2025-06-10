d2694b041c4e8ce81fa93470949cc3114168b0c2
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-06-10 16:57:40 +0300

    feat: add rendering of events in the calendar

--- src/components/EventCalendar/EventCalendar.tsx
+++ src/components/EventCalendar/EventCalendar.tsx
@@ -1,16 +1,28 @@
-import {type FC} from 'react';
-import { Calendar } from 'antd';
-import type { TEvent } from '../../types';
+import { type FC } from "react";
+import { Calendar, type CalendarProps } from "antd";
+import type { TEvent } from "../../types";
+import  { Dayjs } from "dayjs";
+import { formateDate } from "../../utils/formateDate";
 
 type TEventCalsendarProps = {
   events: TEvent[];
+};
 
-}
+const EventCalendar: FC<TEventCalsendarProps> = (props) => {
+  const dateCellRender = (date: Dayjs) => {
+    const formatedDate = formateDate(date.toDate());
+    const currentDayEvents = props.events.filter(ev => ev.date === formatedDate)
+    return (<div>
+        {currentDayEvents.map((ev,i) => <div key={i}>{ev.description}</div> )}
+    </div>);
+  };
 
-const EventCalendar: FC<TEventCalsendarProps> = () => {
-  return (
-    <Calendar/>
-  );
+  const cellRender: CalendarProps<Dayjs>["cellRender"] = (current, info) => {
+    if (info.type === "date") return dateCellRender(current);
+    return info.originNode;
+  };
+
+  return <Calendar cellRender={cellRender} />;
 };
 
 export default EventCalendar;
