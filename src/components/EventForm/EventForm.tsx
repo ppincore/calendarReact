0db52ec45ca62898c8189b2165de5d0fb74fa286
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-06-10 16:57:02 +0300

    feat: add submit action for crate events by form

--- src/components/EventForm/EventForm.tsx
+++ src/components/EventForm/EventForm.tsx
@@ -2,44 +2,68 @@ import { useState, type FC } from "react";
 import { Form, Input, DatePicker, Button, Row, Select } from "antd";
 import { rules } from "../../utils/rules";
 import type { TEvent, TUser } from "../../types";
+import { Dayjs } from "dayjs";
+import { formateDate } from "../../utils/formateDate";
+import { useSelector } from "../../store/store";
+import { selectUserInfo } from "../../slices/sliceStorage/userSlice";
 
 type TEventFormProps = {
   guests: TUser[];
+  submit: (event: TEvent) => void;
 };
 
 const EventForm: FC<TEventFormProps> = (props) => {
-
+  const currentUser = useSelector(selectUserInfo);
   const [event, setEvent] = useState<TEvent>({
     author: "",
     guest: "",
     date: "",
-    descrition: "",
+    description: "",
   });
 
+  const selectDate = (date: Dayjs | null) => {
+    if (date) {
+      setEvent({ ...event, date: formateDate(date?.toDate()) });
+    }
+  };
+
+  const submitForm = () => {
+    props.submit({ ...event, author: currentUser.username });
+  };
+
   return (
-    <Form>
+    <Form onFinish={submitForm}>
       <Form.Item
         label="Event Name"
         name="description"
         rules={[rules.required()]}
       >
-        <Input />
+        <Input
+          onChange={(e) => setEvent({ ...event, description: e.target.value })}
+          value={event.description}
+        />
       </Form.Item>
       <Form.Item label="Event Date" name="date" rules={[rules.required()]}>
-        <DatePicker />
+        <DatePicker
+          onChange={(date: Dayjs) => selectDate(date)}
+        />
       </Form.Item>
       <Form.Item label="Guest" name="guest" rules={[rules.required()]}>
         <Select
-          options={props.guests.map((user) => ({
-            value: user.username,
-            label: user.username,
-          }))}
-          onChange={(guest:string) => setEvent({...event, guest})}
+          options={props.guests
+            .map((user) => ({
+              value: user.username,
+              label: user.username,
+            }))
+            .filter((select) => select.value !== currentUser.username)}
+          onChange={(guest: string) => setEvent({ ...event, guest })}
         />
       </Form.Item>
       <Row justify="end">
         <Form.Item>
-          <Button type="primary">Add Event</Button>
+          <Button type="primary" htmlType="submit">
+            Add Event
+          </Button>
         </Form.Item>
       </Row>
     </Form>
--- src/pages/EventPage/Event.tsx
+++ src/pages/EventPage/Event.tsx
@@ -1,24 +1,31 @@
 import { Layout, Row, Button, Modal } from "antd";
 import { useEffect, useState, type FC } from "react";
 import { useDispatch, useSelector } from "../../store/store";
-import { fetchGuests, selectGuests } from "../../slices/sliceStorage/eventSlice";
+import {
+  fetchGuests,
+  selectGuests,
+  createEvent,
+  fetchEvents,
+} from "../../slices/sliceStorage/eventSlice";
 import EventCalendar from "../../components/EventCalendar/EventCalendar";
 import EventForm from "../../components/EventForm/EventForm";
+import { selectUserInfo } from "../../slices/sliceStorage/userSlice";
 const Event: FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const dispatch = useDispatch()
-  const guests = useSelector(selectGuests)
+  const dispatch = useDispatch();
+  const guests = useSelector(selectGuests);
+  const currentUser = useSelector(selectUserInfo).username;
   const showModal = () => {
     setIsModalOpen(true);
   };
-const onCancel = () => {
+  const onCancel = () => {
     setIsModalOpen(false);
   };
 
-  useEffect(()=>{
-    dispatch(fetchGuests())
-  },[])
-
+  useEffect(() => {
+    dispatch(fetchGuests());
+    dispatch(fetchEvents(currentUser));
+  }, []);
 
   return (
     <Layout>
@@ -32,7 +39,13 @@ const onCancel = () => {
         onCancel={onCancel}
         footer={null}
       >
-        <EventForm guests={guests}/>
+        <EventForm
+          guests={guests}
+          submit={(event) => {
+            dispatch(createEvent(event));
+            setIsModalOpen(false);
+          }}
+        />
       </Modal>
     </Layout>
   );
